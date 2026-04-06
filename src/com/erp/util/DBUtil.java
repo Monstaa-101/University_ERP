9c25620a69556d477182482ec1346cda4baa6e48
Prashant Bhardwaj <prashantbhardwaj77724@gmail.com>
2026-04-06 14:48:50 +0530

    Update DBUtil.java

--- src/com/erp/util/DBUtil.java
+++ src/com/erp/util/DBUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/erp_system";
     private static final String USER = "root";
-    private static final String PASS = "Jod12345";
+    private static final String PASS = "Password";
 
     public static Connection getConnection() throws Exception{
         return DriverManager.getConnection(URL, USER, PASS);
